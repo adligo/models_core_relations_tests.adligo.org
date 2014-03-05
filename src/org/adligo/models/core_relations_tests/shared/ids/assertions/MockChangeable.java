365e9f1f624d37a7fd2c3e5ffc90790ee403bb4d
scott <scott@adligo.com>
2014-03-05 11:12:04 -0600

    new class

---
+++ src/org/adligo/models/core_relations_tests/shared/ids/assertions/MockChangeable.java
@@ -0,0 +1,37 @@
+package org.adligo.models.core_relations_tests.shared.ids.assertions;
+
+import org.adligo.models.core.shared.I_Changeable;
+import org.adligo.models.core.shared.I_StorageIdentifier;
+import org.adligo.models.core.shared.I_StorageInfo;
+import org.adligo.models.core.shared.ValidationException;
+
+public class MockChangeable implements I_Changeable {
+	private I_StorageIdentifier id;
+	private Integer version;
+	private I_StorageInfo storageInfo;
+	
+	public I_StorageIdentifier getId() {
+		return id;
+	}
+	public void setId(I_StorageIdentifier id) {
+		this.id = id;
+	}
+	public Integer getVersion() {
+		return version;
+	}
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+	@Override
+	public I_StorageInfo getStorageInfo() {
+		return storageInfo;
+	}
+	@Override
+	public boolean isStored() throws ValidationException {
+		return false;
+	}
+	public void setStorageInfo(I_StorageInfo storageInfo) {
+		this.storageInfo = storageInfo;
+	}
+	
+}
